5b0236779ee6c6a71fdaa57794129f7fa21a1da6
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-10 00:46:08 +0430

    created Box component

---
+++ src/Component/Box/Box.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card } from 'antd';
+
+
+import Form from '../Form/Form';
+import NumberEmoji from '../NumberEmoji/NumberEmoji';
+
+import './Box.css';
+
+
+
+function Box() {
+  return (
+    <>
+      
+          <Card title={<Form />} bordered={false} style={{ width: 700, height: "50%" }}>
+            i am too bad <NumberEmoji score={-0.9} />
+          </Card>
+        
+    </>
+
+  );
+};
+export default Box;
