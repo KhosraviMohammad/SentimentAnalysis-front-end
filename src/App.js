618f5ee60234689882ef5dce2abff45c635bbb34
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-10 00:46:46 +0430

    Box is used in App commponent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Col, Row } from 'antd';
+
+import Box from './Component/Box/Box'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col xs={2}>
+        <div className='App'>
+          <Box />
+        </div>
+      </Col>
+
+    </Row>
   );
 }
 
